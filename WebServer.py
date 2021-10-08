a069143449141555d54bb500515661657b5e84a1
Marlo Haering <marlo@haering.me>
2021-10-08 20:32:32 +0200

    good things

---
+++ WebServer.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/<float:longitude>/<float:latitude>')
+def points(longitude, latitude):
+    print(f'{longitude} | {latitude}')
+    return render_template("mapchoose.html", latitude=latitude, longitude=longitude)
+
+
+if __name__ == "__main__":
+    app.run()
