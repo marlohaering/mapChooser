d82ab981467ffc21bc9cbcc2b8d96fa528146a98
Marlo Haering <marlo@haering.me>
2021-10-09 19:34:21 +0200

    fixed bing location

--- WebServer.py
+++ WebServer.py
@@ -3,9 +3,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/<float:longitude>/<float:latitude>')
-def points(longitude, latitude):
-    print(f'{longitude} | {latitude}')
+@app.route('/<float:latitude>/<float:longitude>')
+def points(latitude, longitude):
     return render_template("mapchoose.html", latitude=latitude, longitude=longitude)
 
 
